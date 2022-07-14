00645b190645d43f424f23a0cbc30fea7f20a0a4
owenmather <owenmather7@gmail.com>
2022-07-14 03:39:43 +0200

    Update todos - Add base restapi for lambda and some tests

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 # AWS Lambda function for handling TODOs
 import os
 import logging
+from typing import Dict, Union, List
+
+import requests
+import base64
+import json
 
 logger = logging.getLogger()
 logger.setLevel(os.environ.get('LOG_LEVEL', 'INFO'))
@@ -15,38 +20,126 @@ except KeyError as e:
     raise e
 
 
-def lambda_handler(event):
+def lambda_handler(event: Dict):
     logger.debug(event)
+    action = ACTIONS.get(event['httpMethod'], lambda _: response(405))
+    return action(event)
 
-    # check lambda event to check http method
-    if event['httpMethod'] == 'GET':
-        return get_todos()
-    elif event['httpMethod'] == 'POST':
-        return create_todo(event)
-    elif event['httpMethod'] == 'PUT':
-        return update_todo(event)
-    elif event['httpMethod'] == 'DELETE':
-        return delete_todo(event)
+
+def response(status_code: int, body: Union[str, int, Dict, List] = None) -> Dict:
+    default_http_messages = {
+        200: 'OK',
+        201: 'Created',
+        210: 'Deleted',
+        204: 'No Content',
+        400: 'Bad Request',
+        401: 'Unauthorized',
+        403: 'Forbidden',
+        405: 'Method Not Allowed',
+        404: 'Not Found',
+        500: 'Internal Server Error',
+    }
+
+    return {
+        'statusCode': status_code,
+        'body': default_http_messages[status_code] if body is None else json.dumps(body)
+    }
+
+
+def _save_todos(todos: List):
+    #   https://api.github.com/repos/OWNER/REPO/contents/PATH \
+    headers = {'Accept': 'application/vnd.github+json', 'Authorization': 'token {}'.format(GITHUB_TOKEN)}
+    url = 'https://api.github.com/repos/{}/contents/{}?ref={}'.format(GITHUB_REPO, TODO_PATH, BRANCH)
+    body = {"message": "saving todo update",
+            "content": base64.b64encode("\n".join(todos).encode("utf-8")).decode(),
+            "sha": META["sha"]}
+    resp = requests.put(url=url, headers=headers, data=json.dumps(body))
+    resp.raise_for_status()
+    return resp.status_code
+
+
+def delete_todo(event: Dict):
+    todo_id = event.get('pathParameters', {}).get('id', None)
+    if todo_id is None:
+        return response(400, "Must specify a todo id")
+    todos = parse_todos(fetch_todos())
+    if _valid_id(todo_id, todos):
+        todos.pop(int(todo_id) - 1)
+        status = _save_todos(todos)
+        return response(status)
+
+
+def create_todo(event: Dict):
+    todo = event.get('body', {})
+    todos = parse_todos(fetch_todos())
+    todos.append(todo)
+    status = _save_todos(todos)
+    return response(status)
+
+
+def update_todo(event: Dict):
+    pass
+
+
+def get_todos(event: Dict) -> Dict:
+    todo_id = event.get('pathParameters', {}).get('id', None)
+    todos = parse_todos(fetch_todos())
+    if todo_id is not None:
+        return response(200, todos[int(todo_id) - 1]) if _valid_id(todo_id, todos) else response(404)
     else:
-        return {
-            'statusCode': 405,
-            'body': 'Method Not Allowed'
-        }
+        return response(200, todos)
 
 
-def delete_todo(event):
-    pass
+def _valid_id(todo_id: Union[str, int], todos: List) -> bool:
+    """
+    Checks if the todo id is valid. The todo_id is valid if it is a line number
+    :param todo_id:
+    :param todos:
+    :return:
+    """
+    if type(todo_id) == str and not todo_id.isnumeric():
+        return False
+    # Line 1 is headers
+    return len(todos) + 2 >= int(todo_id) > 1
 
 
-def update_todo(event):
-    pass
+def parse_todos(todos: str) -> List:
+    # Parses the todos and returns a List of todos
+    rows = todos.splitlines()
+    return rows
 
 
-def create_todo(event):
-    pass
+def _to_json(todos: str):
+    # Parses the todos and returns a dict of todos
+    rows = todos.splitlines()
+    # First row is always the header
+    header = [row.strip() for row in rows[0].split(',')]
+    result = []
+    for idx, row in enumerate(rows[1:]):
+        # Headers are zipped into a dict each each row as values
+        todo = dict(zip(header, row.split(',')))
+        todo['title'] = todo['title'].strip()
+        todo['description'] = todo['description'].strip()
+        todo['created_date'] = todo['created_date'].strip()
+        todo['completed'] = todo['completed'].strip().lower() in TRUTH_VALUES
+        # Line number in csv is used as the id
+        result.append(todo)
+    return rows
 
 
-def get_todos():
-    pass
+def fetch_todos() -> str:
+    # Connects to GitHub API and fetches todos
+    # Returns a list of todos
+    # using https://docs.github.com/en/rest/repos/contents#get-repository-content
+    headers = {'Accept': 'application/vnd.github+json'}
+    url = 'https://api.github.com/repos/{}/contents/{}?ref={}'.format(GITHUB_REPO, TODO_PATH, BRANCH)
+    resp = requests.get(url=url, headers=headers)
+    resp.raise_for_status()
+    resp_json = resp.json()
+    META["sha"] = resp_json["sha"]
+    return base64.b64decode(resp_json['content']).decode('utf-8')
 
 
+TRUTH_VALUES = ['true', 't', '1', 'y', 'yes']
+ACTIONS = {"GET": get_todos, "POST": create_todo, "PUT": update_todo, "DELETE": delete_todo}
+META = {}
--- test/test_main.py
+++ test/test_main.py
@@ -1,6 +1,45 @@
 from unittest import TestCase
 
+try:
+    from dotenv import load_dotenv
+
+    load_dotenv()
+except ImportError:
+    raise ImportError("dotenv is required to run the tests")
+
+from src.main import fetch_todos, parse_todos, get_todos, create_todo, delete_todo
+
 
 class Test(TestCase):
-    def test_lambda_handler(self):
-        self.fail()
+
+    def test_fetch_todos(self):
+        todos = fetch_todos()
+        with open("../data/todo.csv", "r") as f:
+            local_todos = f.read()
+            self.assertEqual(len(todos), len(local_todos))
+            self.assertEqual(todos, local_todos)
+            print(local_todos)
+
+    def test_parse_todos(self):
+        parsed = parse_todos(todos=fetch_todos())
+        print(parsed)
+
+    def test_missing_get_todos(self):
+        event = {"pathParameters": {"id": "100"}, "httpMethod": "GET"}
+        assert get_todos(event)["statusCode"] == 404
+
+    def test_found_get_all_todos(self):
+        event = {"httpMethod": "GET"}
+        assert get_todos(event)["statusCode"] == 200
+
+    def test_create_todo(self):
+        event = {"body": "Count to infinity again,Count to infinity again,2022-11-01,true", "httpMethod": "POST"}
+        assert create_todo(event)["statusCode"] == 200
+
+    def test_get_todo_by_id(self):
+        event = {"pathParameters": {"id": "2"}, "httpMethod": "GET"}
+        assert get_todos(event)["statusCode"] == 200
+
+    def test_delete_todo(self):
+        event = {"pathParameters": {"id": "3"}, "httpMethod": "DELETE"}
+        assert delete_todo(event)["statusCode"] == 200
